2a5b3ae4b2bf212ddd868048913fed32c17f65ef
Gabriel W. Schneider <70598846+GabrielWSchneider@users.noreply.github.com>
2020-09-05 03:11:43 -0500

    Add logic to redact a message from a file.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -3,3 +3,12 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+
+def redact_message(file, message):
+	censored_file = file.split(message)
+	censored_file = "<redacted>".join(censored_file)
+	
+	return censored_file
+
+email_one = redact_message(email_one, "learning algorithms")
+print(email_one)
