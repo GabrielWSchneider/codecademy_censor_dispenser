1de9d226e952d19a7d7f5dcbc033443f9fc0528b
Gabriel W. Schneider <70598846+GabrielWSchneider@users.noreply.github.com>
2020-09-06 14:51:40 -0500

    Add logic to determine whether a file requires additional censorship, and censor it.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -6,9 +6,10 @@ email_four = open("email_four.txt", "r").read()
 
 proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "concerning", "horrible", "horribly", "questionable"]
+
 def redact_message(file, message):
-	censored_file = file.split(message)
-	censored_file = "<redacted>".join(censored_file)
+	censored_file = file.replace(message, "<redacted>")
 	
 	return censored_file
 
@@ -19,9 +20,27 @@ def redact_phrases(file, list_of_phrases):
 	
 	return censored_file
 
+def reduce_alarmism(file, list_of_proprietary_phrases, list_of_negative_phrases):
+	censored_file = file
+	to_be_censored = list_of_proprietary_phrases
+	count_of_negative_terms = 0
+	
+	for term in list_of_negative_phrases:
+		count_of_negative_terms += file.count(term)
+		if count_of_negative_terms > 1:
+			to_be_censored.append(term)
+	
+	censored_file = redact_phrases(file, to_be_censored)
+	
+	return censored_file
+
 email_one = redact_message(email_one, "learning algorithms")
 print(email_one)
 print("------\n")
 
 email_two = redact_phrases(email_two, proprietary_terms)
 print(email_two)
+print("------\n")
+
+email_three = reduce_alarmism(email_three, proprietary_terms, negative_words)
+print(email_three)
