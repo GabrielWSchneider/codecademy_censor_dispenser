db4ce0cad0896c36d1f42439640922905bdfb4d4
Gabriel W. Schneider <70598846+GabrielWSchneider@users.noreply.github.com>
2020-09-09 22:30:32 -0500

    Add logic to preserve file length, length of censored words, and original case of the file. Squash 2 related bugs.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -8,21 +8,43 @@ proprietary_terms = ["she", "personality matrix", "sense of self", "self-preserv
 
 negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "concerning", "horrible", "horribly", "questionable"]
 
-def redact_message(file, message):
-	censored_file = file.replace(message, "<redacted>")
+def convert_to_string(lst):
+	string_of_list_contents = ""
+	for item in lst:
+		string_of_list_contents += item
 	
-	return censored_file
+	return string_of_list_contents
 
-def redact_phrases(file, list_of_phrases):
+def restore_case(original_file, censored_file):
+	lst1, lst2 = list(original_file), list(censored_file)
+	
+	for index in range(len(lst2)):
+		if lst2[index] == "#":
+			lst1[index] = lst2[index]
+	
+	case_preserved = convert_to_string(lst1)
+	return case_preserved
+
+def censor_phrase(file, illegal_phrase):
+	replacement_text = "#" * len(illegal_phrase)
+	clean_file = file.lower()
+	clean_file = clean_file.replace(illegal_phrase, replacement_text)
+	clean_file = restore_case(file, clean_file)
+	return clean_file
+
+def censor_phrase_list(file, list_of_phrases):
 	censored_file = file
-	for illegal_phrase in list_of_phrases:
-		censored_file = redact_message(censored_file, illegal_phrase)
+	sorted_list_of_phrases = sorted(list_of_phrases, key=len, reverse=True)
+	for illegal_phrase in sorted_list_of_phrases:
+		censored_file = censor_phrase(censored_file, illegal_phrase)
 	
 	return censored_file
 
 def reduce_alarmism(file, list_of_proprietary_phrases, list_of_negative_phrases):
 	censored_file = file
-	to_be_censored = list_of_proprietary_phrases
+	to_be_censored = []
+	for term in list_of_proprietary_phrases:
+		to_be_censored.append(term)
 	count_of_negative_terms = 0
 	
 	for term in list_of_negative_phrases:
@@ -30,15 +52,16 @@ def reduce_alarmism(file, list_of_proprietary_phrases, list_of_negative_phrases)
 		if count_of_negative_terms > 1:
 			to_be_censored.append(term)
 	
-	censored_file = redact_phrases(file, to_be_censored)
+	to_be_censored_sorted = sorted(to_be_censored, key=len, reverse=True)
+	censored_file = censor_phrase_list(file, to_be_censored_sorted)
 	
 	return censored_file
 
-email_one = redact_message(email_one, "learning algorithms")
+email_one = censor_phrase(email_one, "learning algorithms")
 print(email_one)
 print("------\n")
 
-email_two = redact_phrases(email_two, proprietary_terms)
+email_two = censor_phrase_list(email_two, proprietary_terms)
 print(email_two)
 print("------\n")
 
